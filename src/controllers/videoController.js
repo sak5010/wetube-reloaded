065373bbfe9b0f0fb28f75e45e940eeb99574b7f
sak5010 <sak5010@naver.com>
2022-06-07 23:50:49 +0900

    first commit

---
+++ src/controllers/videoController.js
@@ -0,0 +1,3 @@
+export const recommended = (req, res) => res.send("Home Videos");
+export const watch = (req, res) => res.send("Watch Video");
+export const edit = (req, res) => res.send("Edit Video");
