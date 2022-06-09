87b267c2628511a81536e8c635550cb6f67ed174
sanghyeonKim <sak5010@naver.com>
2022-06-09 20:37:02 +0900

    ~#5 TEMPLATES

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,32 @@
-const fakeUser = {
-  username: "jola",
-  loggedIn: false,
+export const recommended = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minitues ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minitues ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minitues ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
 };
-
-export const recommended = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
