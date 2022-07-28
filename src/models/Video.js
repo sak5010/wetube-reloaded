047258d080dcb9090f6a004c8cd87c4da782e61c
sanghyeonKim <sak5010@naver.com>
2022-07-28 22:41:54 +0900

    ~ #6.18

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, minLength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String }],
   meta: {
