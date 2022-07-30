3820b492bc10c67c06d5a3bad4e45c65559b8e6d
sanghyeonKim <sak5010@naver.com>
2022-07-30 20:52:47 +0900

    ~#6.24 Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,9 +31,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -46,7 +44,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
