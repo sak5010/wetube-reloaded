196db570472337ce66f641975f9a36f2d4a90ace
sanghyeon <sak5010@naver.com>
2023-02-24 00:06:26 +0900

    ~#7.10

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from 'express-session';
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from './middlewares';
 
 const app = express();
 const logger = morgan("dev");
@@ -16,7 +17,8 @@ app.use(session({
   secret: "Hello!",
   resave: true,
   saveUninitialized: true,
-}))
+}));
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
