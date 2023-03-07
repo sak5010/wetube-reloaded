924d11bfc37fdc4a18efae960c6f14e4d25f52ff
sanghyeon <sak5010@naver.com>
2023-03-07 11:45:04 +0900

    #8.8 Static Files

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ app.use(session({
   store: MongoStore.create({ mongoUrl: process.env.DB_URL})
 }));
 app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
