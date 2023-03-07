7fdd81f150fb5ac6b2ceb489dff451549df52b32
sanghyeon <sak5010@naver.com>
2023-03-07 13:09:11 +0900

    ~#8.9 Video Upload

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,19 +9,25 @@ import {
   getChangePassword,
   postChangePassword,
 } from "../controllers/userController";
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles } from "../middlewares";
+import {
+  avatarUpload,
+  protectorMiddleware,
+  publicOnlyMiddleware,
+} from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit")
-        .all(protectorMiddleware)
-        .get(getEdit)
-        .post(uploadFiles.single("avatar"), postEdit);
-userRouter.route("/change-password")
-        .all(protectorMiddleware)
-        .get(getChangePassword)
-        .post(postChangePassword);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(avatarUpload.single("avatar"), postEdit);
+userRouter
+  .route("/change-password")
+  .all(protectorMiddleware)
+  .get(getChangePassword)
+  .post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
