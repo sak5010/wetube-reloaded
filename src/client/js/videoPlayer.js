3568dbfe755038f73aef4d0731d8455bc475fe01
sak5010 <sak5010@naver.com>
2023-03-10 15:07:23 +0900

    #11.1 Play Puase

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,23 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.querySelector("#play");
+const muteBtn = document.querySelector("#mute");
+const time = document.querySelector("#time");
+const volume = document.querySelector("#volume");
+
+const handlePlayandPause = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handleMute = (e) => {};
+
+const handlePause = (e) => (playBtn.innerText = "Play");
+const handlePlay = (e) => (playBtn.innerText = "Pause");
+
+playBtn.addEventListener("click", handlePlayandPause);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
