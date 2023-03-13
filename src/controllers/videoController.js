ba838a1c29aeef7d16ae056f7299d0d211ad6da2
sanghyeon <sak5010@naver.com>
2023-03-14 00:12:57 +0900

    #12.0 Register View Controller

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -106,3 +106,14 @@ export const search = async (req, res) => {
   }
   return res.render("search", { pageTitle: "Search", videos });
 };
+
+export const addView = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.status(404);
+  }
+  video.meta.views = video.meta.views + 1;
+  await video.save();
+  return res.status(200);
+};
