018dfefd94319518f5909a7467d2036b540366ae
sanghyeon <sak5010@naver.com>
2023-03-24 23:41:51 +0900

    add temp comment

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -11,3 +11,4 @@ const Comment = mongoose.model("Comment", commentSchema);
 export default Comment;
 
 // Comment 모델을 만들어주고 User와 Video모델도 Comment와 관계를 맺으므로 수정해주었음.
+// #16.2 pug HTML Template 작업과 #16.3~ 4에서 라우팅 작업을 할듯? 이후에 컨트롤러 만들고.
