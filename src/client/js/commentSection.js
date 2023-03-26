9c38545df234799fc873481c90addf7945c55f68
sanghyeon <sak5010@naver.com>
2023-03-26 21:06:10 +0900

    rest of them

---
+++ src/client/js/commentSection.js
@@ -0,0 +1,24 @@
+const videoContainer = document.querySelector("#videoContainer");
+const form = document.querySelector("#commentForm");
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const textarea = form.querySelector("textarea");
+  const text = textarea.value;
+  const videoId = videoContainer.dataset.id;
+  if (text === "") {
+    return;
+  }
+  await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+  textarea.value = "";
+};
+
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
